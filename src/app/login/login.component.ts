5d43dd558784d794b2f6b708a1b5fbe344340052
Sandhya-1728 <sandisoma17@gmail.com>
2023-03-25 06:37:14 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  type: string = "password"
-  isText: boolean =false;
-  eyeIcon: string = "fa-eye-slash";
-  constructor() { }
-
-  ngOnInit(): void{
+  constructor(private router: Router) {}
+  ngOnInit(): void {}
+  loginUser(event: any) {
+    event.preventDefault();
+    console.log(event);
+    var username = event.target.elements[0].value;
+    var password = event.target.elements[1].value;
+    if (username == 'admin@gmail.com' && password == 'admin') {
+      this.router.navigate(['/', 'admin']);
+    } else if (username == 'student@gmail.com' && password == 'student') {
+      this.router.navigate(['/', 'student']);
+    }
   }
-
-  hideShowPass(){
-    this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa fa-eye-slash" : this.eyeIcon = "fa fa-eye-slash";
-    this.isText ? this.type ="text" : this.type ="password";
-  }
-
 }
+// export class LoginComponent implements OnInit {
+
+//   type: string = "password"
+//   isText: boolean =false;
+//   eyeIcon: string = "fa-eye-slash";
+//   constructor() { }
+
+//   ngOnInit(): void{
+//   }
+
+//   hideShowPass(){
+//     this.isText = !this.isText;
+//     this.isText ? this.eyeIcon = "fa fa-eye-slash" : this.eyeIcon = "fa fa-eye-slash";
+//     this.isText ? this.type ="text" : this.type ="password";
+//   }
+
+// }
