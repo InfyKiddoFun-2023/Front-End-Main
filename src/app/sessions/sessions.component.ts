ccacecd444b79dc22f3c70a9369c7edab9f15ade
Yashica <yashicachauhan30@gmail.com>
2023-04-04 22:04:57 +0530

    videoscroller

--- src/app/sessions/sessions.component.ts
+++ src/app/sessions/sessions.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sessions.component.scss']
 })
 export class SessionsComponent implements OnInit {
+  videos = [
+    {
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      title: 'Video 1',
+      views: 1000
+    },
+    {
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      title: 'Video 2',
+      views: 2000
+    },
+    {
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      title: 'Video 3',
+      views: 3000
+    },
+    {
+      thumbnail: 'https://via.placeholder.com/10x5',
+      title: 'Video 4',
+      views: 4000
+    },
+    {
+      thumbnail: 'https://via.placeholder.com/10x5',
+      title: 'Video 5',
+      views: 5000
+    },
+    {
+      thumbnail: 'https://via.placeholder.com/10x5',
+      title: 'Video 6',
+      views: 6000
+    },
+    {
+      thumbnail: 'https://via.placeholder.com/10x5',
+      title: 'Video 7',
+      views: 7000
+    },
+    {
+      thumbnail: 'https://via.placeholder.com/10x5',
+      title: 'Video 8',
+      views: 8000
+    }]
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
 
+  }
 }
