c84bc149461f69fe4422d9b9d409b048dbb289fb
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-04-08 16:54:33 +0530

    Create CourseService and StudentCourseService

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { EnvironmentUrlService } from "./http/env-url.service";
 import { PaginatedResult } from "../models/wrapper/paginated-result";
 import { CourseResponse } from "../models/courses/course-response";
+import { CourseFullResponse } from "../models/courses/course-full-response";
 
 @Injectable({
     providedIn: "root"
@@ -13,15 +14,24 @@ export class StudentCourseService extends HttpClientService {
         super(envUrl);
     }
 
-    getStudentCourses(pageNumber: number, pageSize: number, searchString: string) {
+    getCourses(pageNumber: number, pageSize: number, searchString: string) {
         return this.httpClient.get<PaginatedResult<CourseResponse>>(            
-            this.getRoute('api/student/courses'),
+            this.getRoute('api/courses'),
             {
                 headers: this.getHeaders(),
                 params: new HttpParams()
                     .set('pageNumber', pageNumber.toString())
                     .set('pageSize', pageSize.toString())
                     .set('searchString', searchString)
+            }
+        );
+    }
+
+    getCourse(id: String) {
+        return this.httpClient.get<CourseFullResponse>(            
+            this.getRoute('api/courses/' + id),
+            {
+                headers: this.getHeaders()
             }
         );
     }
