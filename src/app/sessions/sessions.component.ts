bc04caa6b0063b5bdde7789c741193d1509027fd
Yashica <yashicachauhan30@gmail.com>
2023-04-08 12:10:07 +0530

    styling

--- src/app/sessions/sessions.component.ts
+++ src/app/sessions/sessions.component.ts
@@ -8,42 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class SessionsComponent implements OnInit {
   videos = [
     {
-      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/5x5',
       title: 'Video 1',
       views: 1000
     },
     {
-      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/5x5',
       title: 'Video 2',
       views: 2000
     },
     {
-      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/30x20',
+      thumbnail: 'https://www.youtube-nocookie.com/embed/6znrAJbRAUY/5x5',
       title: 'Video 3',
       views: 3000
     },
     {
-      thumbnail: 'https://via.placeholder.com/10x5',
+      thumbnail: 'https://via.placeholder.com/5x5',
       title: 'Video 4',
       views: 4000
     },
     {
-      thumbnail: 'https://via.placeholder.com/10x5',
+      thumbnail: 'https://via.placeholder.com/5x5',
       title: 'Video 5',
       views: 5000
     },
     {
-      thumbnail: 'https://via.placeholder.com/10x5',
+      thumbnail: 'https://via.placeholder.com/5x5',
       title: 'Video 6',
       views: 6000
     },
     {
-      thumbnail: 'https://via.placeholder.com/10x5',
+      thumbnail: 'https://via.placeholder.com/5x5',
       title: 'Video 7',
       views: 7000
     },
     {
-      thumbnail: 'https://via.placeholder.com/10x5',
+      thumbnail: 'https://via.placeholder.com/5x5',
       title: 'Video 8',
       views: 8000
     }]
