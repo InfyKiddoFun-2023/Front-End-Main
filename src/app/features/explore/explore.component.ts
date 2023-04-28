980aeeb89fbdf67647b7302e9a90ea6aa71c86e5
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-04-28 06:13:20 +0530

    Remove unused files

--- src/app/features/explore/explore.component.ts
+++ src/app/features/explore/explore.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html'
 })
 export class ExploreComponent {
+  constructor(public courseService: CourseService) { }
 
 }
